11750bfe865afd1bb2863141256c366ff1111eb0
Florian Paul Schmidt <mista.tapas@gmx.net>
2015-11-07 10:23:26 +0100

    playing around

--- example/server.js
+++ example/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var crypto = require('crypto');
+var sha256 = require('crypto-js/sha256');
+var base64 = require('crypto-js/enc-base64');
 
 var app = express();
 
@@ -8,7 +9,7 @@ var prefix = '';
 var challenge_ttl_milliseconds = 30000;
 var prefix_length = 3;
 
-var max_number_of_challenges = 1000;
+var max_number_of_challenges = 10;
 var challenges = [ ];
 
 var max_number_of_messages = 100;
@@ -21,11 +22,11 @@ app.get(prefix + '/challenge', function(req, res) {
         return;
     }
 
-    var shasum = crypto.createHash('sha256');
-    shasum.update('' + Math.random());
-    challenge = shasum.digest('base64').substring(0,  prefix_length);
-    challenges.push({ "challenge": challenge, "date": new Date() });
-    res.send('{ "challenge": "' + challenge + '" }');
+    var challenge = base64.stringify(sha256('' + Math.random()));
+    var prefix = challenge.substring(0,  prefix_length); 
+
+    challenges.push({ "uuid": challenge, "prefix": prefix, "date": new Date() });
+    res.send('{ "id": "' + challenge + ', "prefix": "' + prefix + '" }');
 });
 
 var server = app.listen(9876, function() {
